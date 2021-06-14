df64d72c529c2b29b8e0f7e096164a5ad75fc49f
enzo hinostroza <enzohd_45@outlook.com>
2021-06-14 17:21:35 -0500

    .....

--- HomeSafeApp/src/main/java/pe/edu/upc/repository/CustomerRepository.java
+++ HomeSafeApp/src/main/java/pe/edu/upc/repository/CustomerRepository.java
@@ -1,11 +1,17 @@
 package pe.edu.upc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.entities.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-
+	@Query("select c from Customer c where c.LastName_Customer like %:LastName_Customer%")
+	public List<Customer> findByName(String lastnameCustomer);
+	
+	//public List<Customer> findBynameCustomerLikeIgnoreCase(String name);
 }
